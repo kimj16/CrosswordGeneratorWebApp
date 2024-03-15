e8ba8c027949c26ff791dc04283787f5b800628f
kimj16 <justink1134@gmail.com>
2024-03-15 15:59:35 -0500

    routing from root to builder page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CrosswordBuilderPageComponent } from './crossword-builder-page/crossword-builder-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'crossword-builder-page', component: CrosswordBuilderPageComponent},
+];
